6430c22523694b8ae5a0e68c1acc72d74724eb7a
EdoardoGalati <edoardogalati@gmail.com>
2025-08-15 15:35:43 +0200

    Initial Commit

---
+++ MyPolaroids/MyPolaroidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPolaroidsApp.swift
+//  MyPolaroids
+//
+//  Created by Edoardo Galati on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MyPolaroidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyPolaroidsTests/MyPolaroidsTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyPolaroidsTests.swift
+//  MyPolaroidsTests
+//
+//  Created by Edoardo Galati on 8/15/25.
+//
+
+import Testing
+
+struct MyPolaroidsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
